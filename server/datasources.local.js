991d8a5d5a3e7791fd5fef26262cdccb4bb511fb
TrainmonAnon <36116671+TrainmonAnon@users.noreply.github.com>
2019-05-06 09:49:43 -0700

    Ready to Deploy v4

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,16 +1,11 @@
 module.exports = {
-    'db': {
-      'name': 'db',
-      'connector': 'memory'
-    },
-    'MongoDB': {
-      'host': '',
-      'port': 0,
-      'url': 'mongodb://localhost:27017',
-      'database': '',
-      'password': '',
-      'name': 'MongoDB',
-      'user': '',
-      'connector': 'mongodb',
-    },
+  'db': {
+    'name': 'db',
+    'connector': 'memory',
+  },
+  'MongoDB': {
+    'url': 'mongodb://localhost:27017',
+    'name': 'MongoDB',
+    'connector': 'mongodb',
+  },
 };
